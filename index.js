482c5a330ef2821542bf0627f6f02195602fa644
developjik <developjik@gmail.com>
2021-01-20 01:17:08 +0900

    boiler plate 2021 01 20

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// express
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// key.js => mongodb setting
+const config = require("./config/key");
+
+// mongo db mongoose
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://developjik:Mi12151506!@boiler-plate.so3qz.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -16,10 +21,27 @@ mongoose
   .then(() => console.log("MONGODB CONNECT..."))
   .catch((err) => console.log(err));
 
+// body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// User model
+const { User } = require("./models/User");
+
+//route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/register", (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save((err, userInfo) => {
+    if (err) return res.json({ register: "fail", err });
+    return res.status(200).json({ register: "success" });
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`EXPRESS SERVER CONNECT...`);
 });
